c808c25b1d47e6a989f77f7c7e99bb9656f25134
Enza Rafaela Dev <rafaeladenadai@gmail.com>
2024-04-11 17:08:30 -0300

    Atualizar o app.js

--- app.js
+++ app.js
@@ -7,6 +7,19 @@ app.get('/', (req, res) => {
   res.send('Hello, world! This is a GET request.');
 });
 
+app.get('/authenticateUser', (req, res) => {
+    const username = req.query.username;
+    const password = req.query.password; 
+    
+    if (username === '' || password === '') {
+        res.send('Status Error 400 - Bad Request');
+    } else if (username === 'John Due' && password === '123456') {
+        res.send('Status Success 200 - User Already Exists');
+    } else {
+        res.send('Status Error 404 - User not Found');
+    }
+});
+
 // Endpoint POST
 app.post('/teste', (req, res) => {
   res.send('Hello, world! This is a POST request.');
