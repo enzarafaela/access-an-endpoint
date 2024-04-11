1181a857292df921a1878696e5adb47c8c237074
Enza Rafaela De Nadai Victal Mariano <rafaeladenadai@gmail.com>
2024-04-11 15:58:06 -0300

    feat: adiciona config

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 // Endpoint GET
 app.get('/', (req, res) => {
@@ -12,6 +11,4 @@ app.post('/teste', (req, res) => {
   res.send('Hello, world! This is a POST request.');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+
