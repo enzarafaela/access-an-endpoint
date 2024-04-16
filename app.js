30a56fd5dd7c8e89df2ede37d7037b73bdbb289b
Enza Rafaela De Nadai Victal Mariano <rafaeladenadai@gmail.com>
2024-04-16 00:49:43 -0300

    feat: ajusta para case squad gerenciamento de pedidos e notificação

--- app.js
+++ app.js
@@ -1,30 +1,63 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
 
-// Endpoint GET
-app.get('/', (req, res) => {
-  res.send('Hello, world! This is a GET request.');
+// Middleware para fazer o parsing do corpo das requisições como JSON
+app.use(bodyParser.json());
+
+// Simulação de um banco de dados de pedidos
+const Aceito = 'Aceito';
+const Preparando = 'Preparando';
+const Transporte = 'Em Transporte';
+
+const status = {
+  Aceito,
+  Preparando,
+  Transporte,
+};
+
+const pedidos = {
+  45800: Object.values(status)[0],
+  45799: Object.values(status)[1],
+  45798: Object.values(status)[2],
+};
+
+// Endpoint para obter o status de um pedido pelo ID
+app.get('/status/:pedidoId', (req, res) => {
+  // Extrai o ID do pedido da URL
+  const pedidoId = req.params.pedidoId;
+
+  // Verifica se o pedido existe no banco de dados
+  if (pedidos[pedidoId]) {
+    // Retorna o status do pedido
+    res.json({ pedidoId, status: pedidos[pedidoId] });
+  } else {
+    // Retorna uma mensagem de erro se o pedido não for encontrado
+    res.status(404).json({ mensagem: 'Pedido não encontrado' });
+  }
 });
 
-app.get('/authenticateUser', (req, res) => {
-    const username = req.query.username;
-    const password = req.query.password; 
-    
-    if (username === '' || password === '') {
-        res.send('Status Error 400 - Bad Request');
-    } else if (username === 'John Due' && password === '123456') {
-        res.send('Status Success 200 - User Already Exists');
-    } else {
-        res.send('Status Error 404 - User not Found');
-    }
+// Endpoint para simular o envio de uma atualização de status para a squad de notificações
+app.post('/atualizar-status', (req, res) => {
+  // Extrai os dados da requisição
+  const pedidoId = req.query.pedido;
+  const novoStatus = req.query.status;
+
+  if (!pedidoId || !pedidos[pedidoId]) {
+    res.status(404).json({ erro: 'Pedido não encontrado' });
+  } else if (!novoStatus) {
+    res.status(400).json({ erro: 'Bad Request - Informe o novo Status' });
+  } else if (!status[novoStatus]) {
+    res.status(400).json({ erro: 'Bad Request - Informe um Status Válido' });
+  } else {
+    // Retorna uma resposta de sucesso
+    res.json({
+      mensagem: `Atualização de status '${novoStatus}' enviada com sucesso para o pedido '${pedidoId}'`,
+    });
+  }
 });
 
-// Endpoint POST
-app.post('/teste', (req, res) => {
-  res.send('Hello, world! This is a POST request.');
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+// Inicia o servidor na porta 3000
+app.listen(3000, () => {
+  console.log('Servidor rodando na porta 3000');
 });
